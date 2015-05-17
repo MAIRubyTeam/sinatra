6f4e9852f12dc13e06beb8f543e5480d220f57b3
sov-87 <afetisov87@gmail.com>
2015-05-17 22:11:33 +0300

    tests by prokopenya

--- test/api_test.rb
+++ test/api_test.rb
@@ -1,18 +1,22 @@
 require File.expand_path '../test_helper.rb', __FILE__
 
-
 class ApiTest < MiniTest::Unit::TestCase
   include Capybara::DSL
   include Rack::Test::Methods
 
   # Capybara.default_driver = :selenium # <-- use Selenium driver
-
   def setup
     Capybara.app = Sinatra::Application.new
   end
 
-    def app
+  def app
     Sinatra::Application
+  end
+
+  def check_fields(id, name)
+    parsed_body = ActiveSupport::JSON.decode(last_response.body)
+    assert_equal parsed_body["id"], id, parsed_body
+    assert_equal parsed_body["name"], name, parsed_body
   end
 
   def home_page
@@ -20,45 +24,39 @@ class ApiTest < MiniTest::Unit::TestCase
     assert page.has_content?('Everybody can see this page')
   end
 
-  def test_entity_save
-  	page.driver.post("/users", name: "petya")
+  def test_entity_create
+    post("/users", name: "petya")
 
-  	assert page.status_code == 200
-   	assert page.html == "{id: 1, name: 'petya'}", page.html
- end
-
- def test_entity_delete
- 	page.driver.submit :delete, "/users/id", {}
-
- 	assert page.status_code == 200
-   	assert has_content?
-
+    assert last_response.status == 200
+    check_fields(1, "petya")
   end
 
-  def test_entity_changed
-  	test_get_entity_id
+  def test_entity_delete
+    delete 'users/1'
 
-  	put '/users/4', name: "kolya"
-  	assert last_response.status == 200
+    assert last_response.status == 200
+  end
 
-  	parsed_body = ActiveSupport::JSON.decode(last_response.body)
-  	assert_equal parsed_body["id"], 4, parsed_body
-  	assert_equal parsed_body["name"], "kolya", parsed_body
+  def test_entity_update
+    test_get_entity_id
+
+    put '/users/4', name: "petya"
+    assert last_response.status == 200
+
+    check_fields(4, "petya")
   end
 
   def test_get_entity_id
-  	get '/users/4'
-  	assert last_response.status == 200
-  	
-  	parsed_body = ActiveSupport::JSON.decode(last_response.body)
-  	assert_equal parsed_body["id"], 4, parsed_body
-  	assert_equal parsed_body["name"], "ivan", parsed_body
-  	
+    get '/users/4'
+    assert last_response.status == 200
+
+    check_fields(4, "ivan")
+
   end
 
   def test_get_entity
-  	get '/users'
-  	assert last_response.status == 200
+    get '/users'
+    assert last_response.status == 200
 
     parsed_body = ActiveSupport::JSON.decode(last_response.body)
     assert_instance_of(Array, parsed_body)
