b8f55d310509f1c31184a4a39a454cf470507958
alexandrov-va <alexandrov.vov@gmail.com>
2015-06-21 20:35:36 +0300

    Привязка типа данных к эдиторам колонок таблицы

--- controllers/extjs_generator.rb
+++ controllers/extjs_generator.rb
@@ -4,10 +4,16 @@ class App < Sinatra::Base
 
 		entity = params[:entity]
 
+		column_editors = {'int' => 'numberfield', 'string' => 'textfield', 'date' => 'datefield'}
+
 		columns = ActiveRecord::Base.connection.columns(entity)
 
 		column_names = columns.map{|column| column.name }
+		column_types = columns.map{|column| column.sql_type.gsub('varchar', 'string').gsub(/[^a-z]/) { '' }}
 
-		erb :app, :locals => {:entity => entity, :columns => column_names}
+		column_array = [column_names, column_types]
+		columns = Hash[*column_array.transpose.flatten]
+
+		erb :app, :locals => {:entity => entity, :columns => columns, :column_editors => column_editors}
 	end
 end
